d156ad7c08c2feed6edd43465326067eff89dcc3
Serhii Dymenko <serge.sergeevich@gmail.com>
2026-05-26 19:28:45 -0400

    Providers AppServiceProvider add RateLimiters

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('api', function (Request $request): Limit {
+            return Limit::perMinute(120)->by((string) ($request->user()?->id ?: $request->ip()));
+        });
+
+        RateLimiter::for('webhooks', function (Request $request): Limit {
+            $provider = (string) $request->route('provider');
+
+            return Limit::perMinute(240)->by($provider.'|'.$request->ip());
+        });
     }
 }
